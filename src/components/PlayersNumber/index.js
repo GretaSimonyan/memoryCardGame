11d5ac24743222ac4dfa6c19139575e11cbe7f90
GretaSS <gsimonyan228@gmail.com>
2021-03-18 17:24:13 +0400

    added default value

--- src/components/PlayersNumber/index.js
+++ src/components/PlayersNumber/index.js
@@ -2,11 +2,10 @@ import React, {useContext} from "react";
 import {
     View
 }from '../../styled';
-import {Context} from '../../Store';
+import { Context } from '../../Store';
 
 function PlayersNumber(){
     const [state, dispatch] = useContext(Context);
-
     let plArr = ["A","B","C","D"];
     plArr.length = state.plValue;
 
@@ -21,7 +20,7 @@ function PlayersNumber(){
         <View id='playersNumber' flex justC='space-between' m='0 auto' w='70%'>
             <View>
                 {
-                    plArr.map((num, key) => 
+                    plArr.map((num=state.plValue, key) => 
                         <View key={key}>
                             Player {num} : 0
                         </View>
