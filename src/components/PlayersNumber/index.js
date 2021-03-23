96fd4dce66785b2322e5e1b693dbe398a416e6b0
GretaSS <gsimonyan228@gmail.com>
2021-03-23 22:03:10 +0400

    changed plArr to curArr

--- src/components/PlayersNumber/index.js
+++ src/components/PlayersNumber/index.js
@@ -6,14 +6,12 @@ import { Context } from '../../Store';
 
 function PlayersNumber(){
     const [state, dispatch] = useContext(Context);
-    let plArr = ["A","B","C","D"];
-    plArr.length = state.plValue;
-      
+
     return(
         <View id='playersNumber' flex justC='space-between' m='0 auto' w='70%'>
             <View>
                 {
-                    plArr.map((num=state.plValue, key) => 
+                    state.curArr.map((num=state.plValue, key) => 
                         <View key={key}>
                             Player {num} : 0
                         </View>
@@ -21,7 +19,7 @@ function PlayersNumber(){
                 }
             </View>
             <View>
-                Current Player:  {state.current}
+                Current Player:  {state.currentPlayer}
             </View>
         </View>
     )
