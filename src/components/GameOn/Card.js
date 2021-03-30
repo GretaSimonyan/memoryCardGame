cde811e2de966f142b0bbe3548683c3872be2299
GretaSS <gsimonyan228@gmail.com>
2021-03-30 22:11:06 +0400

    changed component to styled-component

--- src/components/GameOn/Card.js
+++ src/components/GameOn/Card.js
@@ -1,34 +1,14 @@
-import React, {useContext, useState, useEffect} from "react";
 import {
     View
 }from '../../styled';
-import {Context} from '../../Store';
 import styled from 'styled-components';
 
-// import Pictures from '../Pictures';
-// import cards from './cards';
-
-const Item = styled(View)`
+const Card = styled(View)`
     width: 100px;
     height: 120px;
-    margin: 5px 5px 0 0;
-    ${ (props) => props.isFlipped === false ? `` : 
-    `border: 2px solid darken($accent-color, 10%);
-	transform: rotateY(180deg);` }
+    transition: transform 0.5s;
+    ${ (props) => props.isFlipped === false ? `background: linear-gradient(0deg, rgba(255,33,33,1) 25%, rgba(252,164,41,1) 74%);
+    ` : `transform: rotateY(180deg);` }
 `;
-
-function Card({imgURL, isFlipped, onClick}){
-    return(
-        <>
-            <Item 
-                bgImg={ImgURL}
-                bgSize='cover'
-                bgPos='center'
-                bgRep='no-repeat'
-                // {...props}
-            />
-        </>
-    )
-}
 
 export default Card;
