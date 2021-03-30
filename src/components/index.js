fcc317832387d5def2a44888e77e647e921d241c
GretaSS <gsimonyan228@gmail.com>
2021-03-30 22:15:22 +0400

    added import for navigation component

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Header from '../components/Header';
 import Selects from '../components/Selects';
 import Border from '../components/Border';
+import Navigation from '../components/Navigation'
 
 export{
     Header,
     Selects,
-    Border
+    Border,
+    Navigation
 }
