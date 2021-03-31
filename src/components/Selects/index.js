468c12c5eb30a375d9b5e86611cf09eea321c30d
GretaSS <gsimonyan228@gmail.com>
2021-03-31 23:10:26 +0400

    added imports and tags for some components

--- src/components/Selects/index.js
+++ src/components/Selects/index.js
@@ -3,13 +3,20 @@ import {
     View
 }from '../../styled';
 import PlayersSelect from '../PlayersSelect';
-import BorderSizeSelect from '../BorderSizeSelect';
-
+import BorderHeightSelect from '../BorderHeightSelect';
+import BorderWidthSelect from '../BorderWidthSelect';
+ 
 function Selects(){
     return( 
         <View id='selects' flex alignI='center' justC='space-around' >
             <PlayersSelect />
-            <BorderSizeSelect />
+            <View id='borderSizeSelect' flex fD='column' >
+                <View fontSize='25px'>Border Size</View>
+                <View flex fD='column' alignI='flex-end' justC='center'>
+                    <BorderHeightSelect/>
+                    <BorderWidthSelect/>
+                </View>    
+            </View>
         </View>
     )
 }
