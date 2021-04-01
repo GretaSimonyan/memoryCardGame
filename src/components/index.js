8db2dc446c617a5c6ddb2aae952a784b8c8670c2
GretaSS <gsimonyan228@gmail.com>
2021-04-01 14:00:31 +0400

    moved import for the Navigation component

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
 import Header from '../components/Header';
 import Selects from '../components/Selects';
 import Border from '../components/Border';
-import Navigation from '../components/Navigation'
 
 export{
     Header,
     Selects,
     Border,
-    Navigation
 }
