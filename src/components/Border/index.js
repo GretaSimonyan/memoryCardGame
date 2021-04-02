4f98a4cbf5a20e7c82341fdd3269f4682a7052bd
GretaSS <gsimonyan228@gmail.com>
2021-04-02 21:25:25 +0400

    deleted unnecessary text

--- src/components/Border/index.js
+++ src/components/Border/index.js
@@ -1,14 +1,18 @@
 import React from "react";
-import {
-    View
-} from '../../styled';
+import { View } from '../../styled';
 import PlayersNumber from '../PlayersNumber';
 import GameOn from '../GameOn';
 
 function Border(){
     return(
-        <View id='border' grid gTC='1fr' gTR='50px auto auto' gap='10px 0' alignI='center' justI='center'>
-            <View fontSize='23px' op='0.5'> Border </View>
+        <View id='border' 
+            grid 
+            gTC='1fr' 
+            gap='10px 0' 
+            justI='center'
+            alignI='center' 
+            gTR='50px auto auto' 
+        >
             <PlayersNumber/> 
             <GameOn/>
         </View>
