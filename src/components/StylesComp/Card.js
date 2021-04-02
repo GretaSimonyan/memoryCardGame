b9511c5a19019b14f19ba72d5db5b9c161c94785
GretaSS <gsimonyan228@gmail.com>
2021-04-02 21:30:56 +0400

    added some styles

--- src/components/StylesComp/Card.js
+++ src/components/StylesComp/Card.js
@@ -8,8 +8,9 @@ const Card = styled(View)`
     background-size: contain;
     background-repeat: no-repeat;
     background-position: center;
+    border-radius: 5px;
     ${ (props) => props.isFlipped === false ? `background: linear-gradient(0deg, rgba(255,33,33,1) 25%, rgba(252,164,41,1) 74%);
-    ` : `transform: rotateY(180deg);` }
+    ` : `transform: rotateY(180deg); border: solid 2px orange` }
 `;
 
 export default Card;
