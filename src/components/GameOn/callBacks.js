412ad7369f4f8d4edc68731da1da1ade0a0f255a
GretaSS <gsimonyan228@gmail.com>
2021-04-12 23:46:03 +0400

    moved function 'generate' to 'callBacks'

--- src/components/GameOn/callBacks.js
+++ src/components/GameOn/callBacks.js
@@ -1,3 +1,26 @@
+import cardImages from './cards';
+
+const pictures = cardImages.slice();
+export const generate = function generate(cardsNum) {
+    if( cardsNum % 2 !== 0 ) {
+        pictures.length = 2;
+        alert("Multiple width and height values must be Even number");
+    }else{
+        pictures.length = cardsNum/2;
+    };
+    pictures.push(...pictures);
+
+    let c = pictures.map((image,index) => ({
+        id: image +" "+ index,
+        imageURL: image,
+        isFlipped: false,
+        canFlip: true
+    }));
+    return c.sort(()=> Math.random() - 0.5);
+};
+
+
+
 export const columns = function columns(width){
     let w = '100px 100px'
     for(let i = 1; i <= width-2; i++){
--- src/components/GameOn/index.js
+++ src/components/GameOn/index.js
@@ -5,26 +5,26 @@ import Card from '../StylesComp/Card';
 import cardImages from './cards';
 import Won from '../Won';
 import { SET_CURRENT_PLAYER } from '../../Actions';
+import {columns,rows, generate} from './callBacks';
 
 const pictures = cardImages.slice();
-function generate(cardsNum) {
-    if( cardsNum % 2 !== 0 ) {
-        pictures.length = 2;
-        alert("Multiple width and height values must be Even number");
-        
-    }else{
-        pictures.length = cardsNum/2;
-    };
-    pictures.push(...pictures);
+// function generate(cardsNum) {
+//     if( cardsNum % 2 !== 0 ) {
+//         pictures.length = 2;
+//         alert("Multiple width and height values must be Even number");
+//     }else{
+//         pictures.length = cardsNum/2;
+//     };
+//     pictures.push(...pictures);
 
-    let c = pictures.map((image,index) => ({
-        id: image +" "+ index,
-        imageURL: image,
-        isFlipped: false,
-        canFlip: true
-    }));
-    return c.sort(()=> Math.random() - 0.5);
-};
+//     let c = pictures.map((image,index) => ({
+//         id: image +" "+ index,
+//         imageURL: image,
+//         isFlipped: false,
+//         canFlip: true
+//     }));
+//     return c.sort(()=> Math.random() - 0.5);
+// };
 
 function GameOn(){
     const [state, dispatch] = useContext(Context);
@@ -128,20 +128,20 @@ function GameOn(){
         cardCanFlip(pic.id, false);
     };
     
-    function columns(width){
-        let w = '100px 100px'
-        for(let i = 1; i <= width-2; i++){
-            w += ' 100px'
-        }
-        return w
-    };
-    function rows(height){
-        let r = '120px 120px'
-        for(let i = 1; i <= height-2; i++){
-            r += ' 120px'
-        }
-        return r
-    };
+    // function columns(width){
+    //     let w = '100px 100px'
+    //     for(let i = 1; i <= width-2; i++){
+    //         w += ' 100px'
+    //     }
+    //     return w
+    // };
+    // function rows(height){
+    //     let r = '120px 120px'
+    //     for(let i = 1; i <= height-2; i++){
+    //         r += ' 120px'
+    //     }
+    //     return r
+    // };
 
     return(
         <>
