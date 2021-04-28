32f7a7b50d3a9ffb2964685ba1f36f9268ce29ef
GretaSS <gsimonyan228@gmail.com>
2021-04-28 12:25:13 +0400

    moved some components and changed their names

---
+++ src/components/Selects/BoardHeightSelect.js
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import { View } from '../../styled';
+import { Context } from '../../Store';
+import Select from '../StylesComp/Select';
+import { SET_HEIGHT } from '../../Actions';
+
+function BoardHeightSelect (){
+    const [state, dispatch] = useContext(Context);
+    const heightArr = [2,3,4,5,6,7,8];
+
+    const handleNumberChange = e => {
+        dispatch({
+            type: SET_HEIGHT,
+            height: heightArr[e.target.value]
+        })
+    };
+
+    return(
+        <View grid gTR='1fr 1fr' gap='5px'>
+            <View fontSize='20px'> Height </View>
+            <Select onChange={e => handleNumberChange(e)}>
+                {
+                    heightArr.map((num=state.height,key)=> 
+                        <option key={key} value={key}>
+                            {num}
+                        </option>
+                    )
+                }
+            </Select>
+        </View>
+    )
+}
+
+export default BoardHeightSelect;
---
+++ src/components/Selects/BoardWidthSelect.js
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import { View } from '../../styled';
+import { Context } from '../../Store';
+import Select from '../StylesComp/Select';
+import { SET_WIDTH } from '../../Actions';
+
+function BoardWidthSelect (){
+    const [state, dispatch] = useContext(Context);
+    const widthArr = [2,3,4,5,6,7,8];
+
+    const handleNumberChange = e => {
+        dispatch({
+            type: SET_WIDTH,
+            width: widthArr[e.target.value]
+        })
+    };
+
+    return(
+        <View grid gTR='1fr 1fr' gap='5px'>
+            <View fontSize='20px'> Width </View>
+            <Select onChange={e => handleNumberChange(e)}>
+                {
+                    widthArr.map((num=state.width,key)=> 
+                        <option key={key} value={key}>
+                            {num}
+                        </option>
+                    )
+                }
+            </Select>
+        </View>
+    )
+}
+
+export default BoardWidthSelect;
