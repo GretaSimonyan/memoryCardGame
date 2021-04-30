b7bec2c7e90e69ce456795404380d0eebe32d99d
GretaSS <gsimonyan228@gmail.com>
2021-04-30 22:52:51 +0400

    added Store

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import GlobalStyles from './styled/globalStyles';
 import Home from './pages/Home';
+import Store from './Store';
 
 class App extends Component{
   render(){
     return(
       <>
         <GlobalStyles/>
-        <Home/>
+        <Store>
+          <Home/>
+        </Store>
       </>
     );
   }
