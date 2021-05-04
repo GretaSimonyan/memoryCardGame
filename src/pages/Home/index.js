7bbada29db6b270eed9d645d1cd49fcb4a07729f
GretaSS <gsimonyan228@gmail.com>
2021-05-04 23:54:09 +0400

    moved the Store

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View } from '../../styled';
 import { Route } from 'react-router-dom';
 import { Header, Selects, Board } from '../../components';
 import Link  from '../../components/StylesComp/NavLink';
 // import { NavLink } from 'react-router-dom';
-import Store from '../../Store';
+// import Store from '../../Store';
+import { Context } from '../../Store';
 
 function Home (){
+    const [state, dispatch] = useContext(Context);
+    function handlClick(e) {
+        // dispatch({
+        //     type: 'LOADING',
+        //     load: e.target.value
+        // })
+        console.log(e.target.href)
+    }
     return(
-        <Store>
+        <>
             <Header/>
             <View grid
                 gTC='1fr 1fr'
@@ -20,11 +29,12 @@ function Home (){
             >
                 <Link to='/'>CHANGE SELECTION</Link>
                 <Link to='/board'>PLAY GAME</Link>
+                {/* <a href='#' onClick={(e) => handlClick(e)} >CHANGE SELECTION</a>
+                <a href='/board' onClick={(e) => handlClick(e)} >PLAY GAME</a> */}
             </View>
-
             <Route path='/' exact component={Selects}/>
             <Route path='/board' component={Board}/>
-        </Store>
+        </>
     )
 }
 export default Home;
