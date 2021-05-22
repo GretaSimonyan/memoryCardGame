b5fd0fa82361b316cf7d259c9968c4dec010c445
GretaSS <gsimonyan228@gmail.com>
2021-05-22 22:52:28 +0400

    deleted some functions

--- src/components/GameOn/index.js
+++ src/components/GameOn/index.js
@@ -8,116 +8,111 @@ import Won from '../Won';
 import { columns, rows, generate } from './utils';
 
 function GameOn(){
-    const [state, dispatch] = useContext(Context);
-    
-    let cardsNum = state.height * state.width;
+	const [state, dispatch] = useContext(Context);
 
-    useEffect(()=> {
-        if( cardsNum % 2 !== 0 ) {
-            alert("Multiple width and height values must be Even number");
-        }else{
-            setCards(generate(cardsNum));
-        };
-    },[cardsNum]);
+	let cardsNum = state.height * state.width;
 
-    const [cards, setCards] = useState(cardImages);
-    // let cards = state.cards
-    let firstCard = state.firstCard;
-    let secondCard = state.secondCard;
+	useEffect(()=> {
+		if( cardsNum % 2 !== 0 ) {
+			alert("Multiple width and height values must be Even number");
+		}else{
+			setCards(generate(cardsNum));
+		};
+	},[cardsNum]);
 
-    function cardIsFlipped(cardId, isFlipped) {
+	const [cards, setCards] = useState(cardImages);
+	// let cards = state.cards
+	let firstCard = state.firstCard;
+	let secondCard = state.secondCard;
+
+	const cardIsFlipped = (cardId, isFlipped) => {
 		setCards(prev => prev.map(c => {
-            console.log({...c, isFlipped})
+			// console.log({...c, isFlipped})
 			if (c.id !== cardId)
 				return c;
 			return {...c, isFlipped};
 		}));
 	};
 
-    function cardCanFlip(cardId, canFlip){
-        setCards(prev => prev.map(c=> {
-            if(c.id !== cardId)
-                return c;
-            return {...c, canFlip};
-        }))
-    };
-    
-    function countPoints(){
-        let flipArr = [];
-        cards.map(card => (!card.canFlip) && flipArr.push(card.canFlip) );
-        if (flipArr.length === cards.length) return <Won/>;
-    };
+	const cardCanFlip = (cardId, canFlip) => {
+		setCards(prev => prev.map(c=> {
+			if(c.id !== cardId)
+				return c;
+			return {...c, canFlip};
+		}));
+	};
 
-    function onSuccessGuess() {
-        cardCanFlip(firstCard.id, false);
-        cardCanFlip(secondCard.id, false);
-        cardIsFlipped(firstCard.id, true);
+	function onSuccessGuess() {
+		cardCanFlip(firstCard.id, false);
+		cardCanFlip(secondCard.id, false);
+		cardIsFlipped(firstCard.id, true);
 		cardIsFlipped(secondCard.id, true);
-        dispatch(setFirstCard(null));
-        dispatch(setSecondCard(null));
-        setPointsForCurrPlayer();
-    };
+		dispatch(setFirstCard(null));
+		dispatch(setSecondCard(null));
+		state.activePlayer.point = state.activePlayer.point + 1;
+		dispatch(setActivePlayer(state.activePlayer));
+	};
 
-    function onFailureGuess() {
-        setTimeout(() => {
+	function onFailureGuess() {
+		setTimeout(() => {
 			cardIsFlipped(firstCard.id, false);
 		}, 1000);
 		setTimeout(() => {
 			cardIsFlipped(secondCard.id, false);
 		}, 1200);
 
-        cardCanFlip(firstCard.id, true);
-        cardCanFlip(secondCard.id, true);
+		cardCanFlip(firstCard.id, true);
+		cardCanFlip(secondCard.id, true);
 
-        dispatch(setFirstCard(null));
-        dispatch(setSecondCard(null));
+		dispatch(setFirstCard(null));
+		dispatch(setSecondCard(null));
 
-        setNextPlayer();
-    };
+		setNextPlayer();
+	};
 
-    function setNextPlayer(){
-        let indexOfActivePlayer = state.currentPlayers.indexOf(state.activePlayer);
-        dispatch(setActivePlayer(( indexOfActivePlayer < state.currentPlayers.length-1)? 
-        state.currentPlayers[indexOfActivePlayer+1] : state.currentPlayers[0]))
-    };
+	function setNextPlayer(){
+		let indexOfActivePlayer = state.currentPlayers.indexOf(state.activePlayer);
+		dispatch(setActivePlayer(( indexOfActivePlayer < state.currentPlayers.length-1)?
+		state.currentPlayers[indexOfActivePlayer+1] : state.currentPlayers[0]))
+	};
 
-    function setPointsForCurrPlayer(){
-        state.activePlayer.point = state.activePlayer.point + 1;
-        dispatch(setActivePlayer(state.activePlayer));
-    };
-    
-    useEffect(() => {
+	useEffect(() => {
 		if (!firstCard || !secondCard)
 			return;
 		((firstCard.imageURL === secondCard.imageURL) && (firstCard.id !== secondCard.id)) ? onSuccessGuess() : onFailureGuess();
 	}, [firstCard, secondCard]);
 
-    function handleClick(pic) {
+	function handleClick(pic) {
 		if (!pic.canFlip) return;
-        (firstCard) ? dispatch(setSecondCard(pic)) : dispatch(setFirstCard(pic));
+		(firstCard) ? dispatch(setSecondCard(pic)) : dispatch(setFirstCard(pic));
+		cardIsFlipped(pic.id, true);
+		cardCanFlip(pic.id, false);
+	};
 
-        cardIsFlipped(pic.id, true);
-        cardCanFlip(pic.id, false);
-    };
+	let flipArr = [];
+	cards.map(card => (!card.canFlip) && flipArr.push(card.canFlip));
 
-    return(
-        <>
-            <View id='gameOn' grid gap='5px 5px'
-                gTC={ columns(state.width) }
-                gTR={ rows(state.height) }
-            >
-                {cards.map((pic,index) => 
-                    <Card 
-                        bgImg={pic.imageURL} 
-                        key={pic+index} 
-                        onClick={() => handleClick(pic)}
-                        isFlipped={pic.isFlipped}
-                    />)
-                }
-            </View>
-            {countPoints()}
-        </>
-    )
+	return(
+		<>
+			<View id='gameOn' grid gap='5px 5px'
+				gTC={ columns(state.width) }
+				gTR={ rows(state.height) }
+			>
+				{
+					cards.map((pic,index) =>
+						<Card
+							bgImg={pic.imageURL}
+							key={pic+index}
+							onClick={() => handleClick(pic)}
+							isFlipped={pic.isFlipped}
+						/>)
+				}
+			</View>
+			{
+				(flipArr.length === cards.length) && <Won/>
+			}
+		</>
+	)
 }
 
 export default GameOn;
