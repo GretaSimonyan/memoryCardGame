27ea52c925cf14a385268517049ecb4c9b739318
GretaSS <gsimonyan228@gmail.com>
2021-05-22 22:55:12 +0400

    changed tab size

--- src/components/Won/index.js
+++ src/components/Won/index.js
@@ -5,45 +5,45 @@ import { View } from '../../styled';
 import Button from '../StylesComp/Button.js';
 
 function Won(){
-    const [state, dispatch] = useContext(Context);
+  const [state, dispatch] = useContext(Context);
 
-    let points = [];
-    state.currentPlayers.map((curr)=> points.push(curr.point));
+  let points = [];
+  state.currentPlayers.map((curr)=> points.push(curr.point));
 
-    useEffect(() => {
-        dispatch(setWinner(state.currentPlayers.find(cur => cur.point === Math.max(...points))));
-    },state.currentPlayers);
+  useEffect(() => {
+    dispatch(setWinner(state.currentPlayers.find(cur => cur.point === Math.max(...points))));
+  },[state.currentPlayers]);
 
-    return(
-        <View id='Won' 
-            z='4'
-            w='50%'
-            m='auto'
-            p='20px'
-            bRad='4px'
-            left='0px'
-            right='0px'
-            pos='absolute'
-            tAlign='center'
-            bgColor='orange'
-            overflow='hidden'
-        >
-            <View grid
-                gap='15px'
-                gTR='1fr 1fr auto'
-                justI='center'
-                fontSize='30px'
-            >
-                <View c='azure'> 
-                    Congratulations Player {state.winner.title} 
-                </View>
-                <View c='azure'> 
-                    You win by gaining {state.winner.point} points
-                </View>
-                <Button href='/'> Restart </Button>
-            </View>
+  return(
+    <View id='Won'
+      z='4'
+      w='50%'
+      m='auto'
+      p='20px'
+      bRad='4px'
+      left='0px'
+      right='0px'
+      pos='absolute'
+      tAlign='center'
+      bgColor='orange'
+      overflow='hidden'
+    >
+      <View grid
+        gap='15px'
+        gTR='1fr 1fr auto'
+        justI='center'
+        fontSize='30px'
+      >
+        <View c='azure'>
+          Congratulations Player {state.winner.title}
         </View>
-    )
+        <View c='azure'>
+          You win by gaining {state.winner.point} points
+        </View>
+        <Button href='/'> Restart </Button>
+      </View>
+    </View>
+  )
 }
 
 export default Won;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,27 +6,27 @@ import { Header, Selects, Board } from '../../components';
 import Link  from '../../components/StylesComp/Link';
 
 function Home (){
-    const [state, dispatch] = useContext(Context);
-    function handlClick(e) {
-        dispatch(setView(e.target.outerText));
-    }
+  const [state, dispatch] = useContext(Context);
+  function handlClick(e) {
+    dispatch(setView(e.target.outerText));
+  }
 
-    return(
-        <>
-            <Header/>
-            <View grid
-                gTC='1fr 1fr'
-                m='20px auto' 
-                justI='center'
-                gap='20px 20px'
-                fontSize='14px'
-                alignI='stretch'
-            >
-                <Link href='/selects' onClick={(e) => handlClick(e)} >CHANGE SELECTION</Link>
-                <Link href='/board' onClick={(e) => handlClick(e)} >PLAY GAME</Link>
-            </View>
-            {(state.view === 'CHANGE SELECTION') ? <Selects/> : <Board/>}
-        </>
-    )
+  return(
+    <>
+      <Header/>
+      <View grid
+        gTC='1fr 1fr'
+        m='20px auto'
+        justI='center'
+        gap='20px 20px'
+        fontSize='14px'
+        alignI='stretch'
+      >
+        <Link href='/selects' onClick={(e) => handlClick(e)} >CHANGE SELECTION</Link>
+        <Link href='/board' onClick={(e) => handlClick(e)} >PLAY GAME</Link>
+      </View>
+      {(state.view === 'CHANGE SELECTION') ? <Selects/> : <Board/>}
+    </>
+  )
 }
 export default Home;
