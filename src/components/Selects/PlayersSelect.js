667e2d5ddb4d8ccc6143e832b3de00f07eb08583
greta <gsimonyan228@gmail.com>
2021-05-24 18:04:01 +0300

    changed the logic for setting a random player

--- src/components/Selects/PlayersSelect.js
+++ src/components/Selects/PlayersSelect.js
@@ -3,44 +3,45 @@ import { View } from '../../styled';
 import { Context } from '../../Store';
 import Select from '../StylesComp/Select';
 import { 
-    setPlayersValue,
-    setActivePlayer,
-    setCurrentPlayers
+  setPlayersValue,
+  setActivePlayer,
+  setCurrentPlayers
 } from '../../Actions';
 
 function PlayersSelect (){
-    const [state, dispatch] = useContext(Context);
-    const players = [1,2,3,4]
+  const [state, dispatch] = useContext(Context);
+  const players = [1,2,3,4]
     
-    let arr = state.players.slice();
-    arr.length = state.playersValue;
+  const statePlayers = [...state.players];
+  statePlayers.length = state.playersValue;
 
-    const handleNumberChange = e => {
-        dispatch(setPlayersValue(players[e.target.value]));
-        dispatch(setActivePlayer(arr[Math.floor(Math.random()*arr.length)]))
-    };
+  const handleNumberChange = e => {
+    dispatch(setPlayersValue(players[e.target.value]));
+    const randomPlayer = statePlayers.sort(() => 0.5 - Math.random())[0];
+    dispatch(setActivePlayer(randomPlayer));
+  };
 
-    function currArrChanged(){
-        dispatch(setCurrentPlayers(arr))
-    };
+  function currArrChanged(){
+    dispatch(setCurrentPlayers(statePlayers));
+  };
 
-    return( 
-        <View grid gap='15px'>
-            <View fontSize='25px'> Players Number </View>
-            <Select
-                onChange={handleNumberChange} 
-                onClick={currArrChanged}
-            >
-                {
-                    players.map((num, key) => 
-                        <option key={key} value={key}>
-                            {num}
-                        </option>
-                    )
-                }
-            </Select>
-        </View>
-    )
+  return( 
+    <View grid gap='15px'>
+      <View fontSize='25px'> Players Number </View>
+      <Select
+        onChange={handleNumberChange} 
+        onClick={currArrChanged}
+      >
+        {
+          players.map((num) => 
+            <option key={num} value={num}>
+              {num}
+            </option>
+          )
+        }
+      </Select>
+    </View>
+  )
 }
 
 export default PlayersSelect;
